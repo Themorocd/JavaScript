d51268b89bce37a5f8abdc1bad627bb6f2cf09d8
Themorocd <73909321+Themorocd@users.noreply.github.com>
2023-10-31 10:12:07 +0100

    .

--- 3ºTema/12ºActividad(16)/calculadora.js
+++ 3ºTema/12ºActividad(16)/calculadora.js
@@ -18,7 +18,7 @@ function openCalculator() {
 
 
             setInterval(function() {
-                var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+                let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
                 myWindow.document.body.style.backgroundColor = randomColor;
             }, 3000);
     };
